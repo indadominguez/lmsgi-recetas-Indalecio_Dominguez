b17ef378942fff6f9c876c12c36116fb17204b22
dhindalecio <dhindalecio@iesrafaelalberti.es>
2026-04-27 13:58:02 +0200

    En verdad esto no debería de ir en la App pero por tema de tiempo lo voy a dejar ahí y voy a crear otro js llamado

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const xml2js = require('xml2js');
 
 const xml = fs.readFileSync('recetas.xml', 'utf-8');
 
-
 xml2js.parseString(xml, (error, result) => {
     if (error) {
         console.error(error);
         return;
     }
 
+    const recetas = result.recetas.receta.map(r => ({
+        codigo: r.$.codigo,
+        nombre: r.nombre[0],
+        categoria: r.categoria[0],
+        tiempo: parseInt(r.tiempo[0]),
+        dificultad: r.dificultad[0]
+    }));
 
+    fs.writeFileSync('recetas.json', JSON.stringify(recetas, null, 2));
+
+    console.log("JSON limpio generado correctamente");
 });
